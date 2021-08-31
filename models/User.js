b0af52c2624b4be04060cb506820726f642510ec
Amna Syeda <amna.syeda98@gmail.com>
2021-08-31 18:23:19 -0400

    created models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,44 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    { 
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+\@.+\..+/]
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+  );
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
